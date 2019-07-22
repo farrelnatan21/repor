40f696960179c6ef902b592dcb763cc67ff2df27
triyasniko <triyasniko@gmail.com>
2019-07-22 11:05:00 +0700

    update keterangan

--- repor/pages/admin/pages/kasir.php
+++ repor/pages/admin/pages/kasir.php
@@ -1,113 +1,80 @@
 <section id="main-content">
-      <section class="wrapper">
-        <h3><i class="fa fa-angle-right"></i>TRANSAKSI</h3>
-        <div class="row">
-          
-          
-          <!-- /col-md-12 -->
-          <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-    Topik
-  </button>
-  <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-    <a class="dropdown-item" href="?p=kasir">Seluruh</a>
-    <a class="dropdown-item" href="?p=kakipalsukas">Kaki Palsu</a>
-    <a class="dropdown-item" href="?p=tmmkas">Tangan Mekanik Metekarpal</a>
-    <a class="dropdown-item" href="?p=esg3kas">Exo Skeleton Gen 3</a>
-    <a class="dropdown-item" href="?p=kakipelarikas">Kaki Pelari</a>
-    <a class="dropdown-item" href="?p=morfologikas">Morfologi Scanning Kaki</a>
-    <a class="dropdown-item" href="?p=pcukas">PCU</a>
-    <a class="dropdown-item" href="?p=ceskas">Control Exo Skeleton</a>
-  </div>
-</div>
-          <!-- /col-md-12 -->
+  <section class="wrapper">
+    <h3><i class="fa fa-angle-right"></i>TRANSAKSI</h3>
+    <div class="row">
+      <!-- /col-md-12 -->
+      <div class="col-md-12">
+        <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Topik
+        </button>
+        <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+          <a class="dropdown-item" href="?p=kasir">Seluruh</a>
+          <a class="dropdown-item" href="?p=kakipalsukas">Kaki Palsu</a>
+          <a class="dropdown-item" href="?p=tmmkas">Tangan Mekanik Metekarpal</a>
+          <a class="dropdown-item" href="?p=esg3kas">Exo Skeleton Gen 3</a>
+          <a class="dropdown-item" href="?p=kakipelarikas">Kaki Pelari</a>
+          <a class="dropdown-item" href="?p=morfologikas">Morfologi Scanning Kaki</a>
+          <a class="dropdown-item" href="?p=pcukas">PCU</a>
+          <a class="dropdown-item" href="?p=ceskas">Control Exo Skeleton</a>
         </div>
-        <!-- row -->
-        <div class="row mt">
-          <div class="col-md-12">
-            <div class="content-panel">
-              <table class="table table-striped table-advance table-hover">
-                <h4><i class="fa fa-angle-right"></i><a href="pages/kasirexcel.php" target="_blank"> CETAK EXCEL</h4></a>
-                <hr>
-                <thead>
-                  <tr>
-                    <th>no</th>
-                    <th>nama</th>
-                    <th>topik</th>
-                    <th>tanggal transaksi</th>
-                    <th>jumlah barang</th>
-                    <th>harga satuan</th>
-                    <th>harga total</th>
-                    <th>nota</th>
-                    <th>keterangan</th>
-                    <th>aksi</th>
-                  </tr>
-                </thead>
-                <?php
-                include '../../config/koneksi.php';
-                
+      </div>
+      <!-- /col-md-12 -->
+    </div>
+    <!-- row -->
+    <div class="row mt">
+      <div class="col-md-12">
+        <div class="content-panel">
+          <table class="table table-advance table-hover table-responsive">
+            <h4><i class="fa fa-angle-right"></i><a href="pages/kasirexcel.php" target="_blank"> CETAK EXCEL</h4></a>
+            <hr>
+            <thead>
+              <tr>
+                <th>no</th>
+                <th>nama</th>
+                <th>topik</th>
+                <th>tanggal transaksi</th>
+                <th>jumlah barang</th>
+                <th>harga satuan</th>
+                <th>harga total</th>
+                <th>nota</th>
+                <th>keterangan</th>
+                <th>aksi</th>
+              </tr>
+            </thead>
+            <?php
+            include '../../config/koneksi.php';
+
             $no = 0;
             $data=$koneksi->query("SELECT * FROM t_kasir");
             while($m=mysqli_fetch_array($data)){
-            $no++;    
-          ?>  
-                <tbody>
-                  <tr>
-                    <td scope="row"><?php echo $no;?></a>
-                    </td>
-                    <td><?php echo $m['nama']; ?></td>
-                    <td><?php echo $m['topik']; ?></td>
-                    <td><?php echo $m['tgl_transaksi']; ?></td>
-                    <td><?php echo $m['jumlah_barang'];?></td>
-                    <td><?php echo $m['harga_satuan'];?></td>
-                    <td><?php echo $m['harga_total'];?></td>
-                    <td><img src="../../function/user/nota/<?php echo $m['nota'];?>" height="50"></td>
-                    <td><?php echo $m['keterangan'];?></td>
-                    <td>
-                      <!-- Button untuk modal -->
-                      <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">Cetak</button>
-                    </td>
-                  </tr>
-                  <form action="pages/editkasir.php" method="post">
-<!-- Modal -->
-<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-  <div class="modal-dialog" role="document">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-          <span aria-hidden="true">&times;</span>
-        </button>
-      </div>
-      <div class="modal-body">
-      <div class="form-group">
-    <label for="Jk">Jenis kelamin</label>
-    <select name="jk" class="form-control" id="jk" name="jk" required>
-    	<option value="<?php echo $e['jk'];?>"><?php echo $e['jk'];?></option>
-    	<option value="Pria">Pria</option>
-    	<option value="Wanita">Wanita</option>
-    </select>
-  </div>       
-      </div>
-      <div class="modal-footer">
-      <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-        <button class="btn btn-primary" type="submit" name="submit" value="proses" onclick="return valid();">Print</button>
-      </div>
-    </div>
-  </div>
-</div>              
-</form>
-                  
-                 <?php
-        }
-                 ?>
-                </tbody>
-              </table>
-            </div>
-            <!-- /content-panel -->
-          </div>
-          <!-- /col-md-12 -->
+              $no++;    
+              ?>  
+              <tbody>
+                <tr>
+                  <td scope="row"><?php echo $no;?></a></td>
+                  <td><?php echo $m['nama']; ?></td>
+                  <td><?php echo $m['topik']; ?></td>
+                  <td><?php echo $m['tgl_transaksi']; ?></td>
+                  <td><?php echo $m['jumlah_barang'];?></td>
+                  <td><?php echo $m['harga_satuan'];?></td>
+                  <td><?php echo $m['harga_total'];?></td>
+                  <td><img src="../../function/user/nota/<?php echo $m['nota'];?>" height="50"></td>
+                  <td><?php echo $m['keterangan'];?></td>
+                  <td>
+                    <a class="btn btn-primary" href="../../pages/admin/pages/uptksr_sdh.php?id=<?php echo $m['id']; ?>">sudah transfer</a>
+                    <a class="btn btn-warning" href="../../pages/admin/pages/uptksr_blm.php?id=<?php echo $m['id']; ?>">belum transfer</a>
+                  </td>
+                </tr>
+                <?php
+              }
+              ?>
+            </tbody>
+          </table>
         </div>
-        <!-- /row -->
-      </section>
-    </section>
-    
+        <!-- /content-panel -->
+      </div>
+      <!-- /col-md-12 -->
+    </div>
+    <!-- /row -->
+  </section>
+</section>
---
+++ repor/pages/admin/pages/uptksr_sdh.php
@@ -0,0 +1,20 @@
+
+<?php 
+	include '../../../config/koneksi.php';
+	$id=$_GET["id"];
+
+	$sql=$koneksi->query("UPDATE t_kasir SET keterangan='sudah transfer' WHERE id='$id' ") or die(mysqli_error($koneksi));
+	if ($sql) {
+		echo "
+			<script>
+				alert ('update data berhasil');
+				location='../../../pages/admin/index.php';
+			</script>";
+	}else{
+		echo "
+			<script>
+				alert ('update data gagal');
+				location='../../../pages/admin/index.php';
+			</script>";
+	}
+?>
